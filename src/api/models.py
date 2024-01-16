58dc1682fe60b5d3498c47fbee0af50dd3a01be3
Lourdes Infante <117685514+LourInf@users.noreply.github.com>
2024-01-16 11:00:00 +0000

    add relationships

--- src/api/models.py
+++ src/api/models.py
@@ -6,6 +6,7 @@ db = SQLAlchemy()
 
 
 class Users(db.Model):
+    __tablename__ = "users"
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(80), unique=False, nullable=False)
@@ -26,8 +27,9 @@ class Patients(db.Model):
     __tablename__ = "patients"
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(50), nullable=False)
-    users_id = db.Column(db.Integer, db.ForeingKey('users.id'), unique=True)
-    users = relationship(Users)
+    
+    users_id = db.Column(db.Integer, db.ForeignKey('users.id'), unique=True)
+    users = db.relationship(Users)
 
     def __repr__(self):
         return f'<Patient {self.username}>'
@@ -40,13 +42,14 @@ class Pharmacies(db.Model):
     __tablename__ = "pharmacies"
     id = db.Column(db.Integer, primary_key=True)
     pharmacy_name = db.Column(db.String(50), nullable=False)
-    SOE_pharmacy_number = db.Column(db.Integer, unique=False)
-    address = db.Column(db.String(), nullable=False)
-    phone = db.Column(db.Integer, nullable=False)
+    SOE_pharmacy_number = db.Column(db.String(20), unique=False)
+    address = db.Column(db.String(150), nullable=False)
+    phone = db.Column(db.String, nullable=False)
     opening_hours = db.Column(db.DateTime)
     closing_hours = db.Column(db.DateTime)
-    users_id = db.Column(db.Integer, db.ForeingKey('users.id'), unique=True)
-    users = relationship(Users)
+    
+    users_id = db.Column(db.Integer, db.ForeignKey('users.id'), unique=True)
+    users = db.relationship(Users)
 
     def __repr__(self):
         return f'<Pharmacy {self.pharmacy_name}>'
@@ -80,12 +83,15 @@ class Orders(db.Model):
     requested_date = db.Column(db.DateTime, default=datetime.utcnow)
     validity_date = db.Column(db.DateTime) # Máximo 24/48h
     order_status = db.Column(db.String(50))
-    patient_id = db.Column(db.Integer, db.ForeingKey('patients.id')) # Preguntar many to one*
-    patients = relationship(Patients)
-    pharmacy_id = db.Column(db.Integer, db.ForeingKey('pharmacies.id')) 
-    pharmacy = relationship(Patients)
-    medicine_id = db.Column(db.Integer, db.ForeingKey('medicine.id')) 
-    medicine = relationship(Patients)
+    
+    patient_id = db.Column(db.Integer, db.ForeignKey('patients.id'))
+    patient = db.relationship(Patients, backref='orders') 
+    
+    pharmacy_id = db.Column(db.Integer, db.ForeignKey('pharmacies.id')) 
+    pharmacy = db.relationship(Pharmacies, backref='orders')
+
+    medicine_id = db.Column(db.Integer, db.ForeignKey('medicines.id')) 
+    medicine = db.relationship(Medicines, backref='orders')
 
     def __repr__(self):
         return f'<Orders {self.order_status}>'
@@ -95,25 +101,26 @@ class Orders(db.Model):
                 'order_status': self.order_status,
                 'order_quantity': self.order_quantity,
                 'requested_date': self.requested_date,
-                'validity_date': self.validity_date
-                }
+                'validity_date': self.validity_date}
 
 class Availability(db.Model):
     __tablename__ = "availability"
     id = db.Column(db.Integer, primary_key=True)
-    avilability_status = db.Column(db.String(50))
+    avilability_status = db.Column(db.String(50), nullable=False)
     updated_date = db.Column(db.DateTime, default=datetime.utcnow)
-    pharmacy_id = db.Column(db.Integer, db.ForeingKey('pharmacies.id'))  # Preguntar 
-    pharmacy = relationship(Patients)
-    medicine_id = db.Column(db.Integer, db.ForeingKey('medicine.id')) 
-    medicine = relationship(Patients)
+    
+    pharmacy_id = db.Column(db.Integer, db.ForeignKey('pharmacies.id')) 
+    pharmacy = db.relationship(Pharmacies, backref='availability')
+    
+    medicine_id = db.Column(db.Integer, db.ForeignKey('medicine.id')) 
+    medicine = db.relationship(Medicines, backref='availability')
 
     def __repr__(self):
         return f'<Availability {self.availability_status}>'
 
     def serialize(self):
         return {'id': self.id,
-                'avilability_status': self.avilability_status,
+                'availability_status': self.availability_status,
                 'updated_date': self.updated_date}
 
 
