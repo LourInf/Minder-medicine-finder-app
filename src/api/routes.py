2e707f33fe55f1c2adf838501e3706be38c698b9
Andleus <abadiaandres@gmail.com>
2024-01-30 21:00:37 +0100

    Clean code, and patients added

--- src/api/routes.py
+++ src/api/routes.py
@@ -2,7 +2,7 @@
 This module takes care of starting the API Server, Loading the DB and Adding the endpoints
 """
 from flask import Flask, request, jsonify, Blueprint
-from api.models import db, Users
+from api.models import db, Users, Patients
 from api.utils import generate_sitemap, APIException
 from flask_cors import CORS
 from flask_jwt_extended import create_access_token
@@ -47,21 +47,36 @@ def login_user():
 @api.route('signup', methods=['POST'])
 def signup():
     data = request.get_json()
-    if 'email' not in data or 'password' not in data:
-        return jsonify({"error": "Where are my requirements?"})
+    if 'email' not in data or 'password' not in data or 'is_pharmacy' not in data:
+        return jsonify({"error": "Where are my requirements?"}), 400
     
-    if data["is_pharmacy"] is True or data["is_pharmacy"] is False:
-        new_user = Users(email=data['email'], password=data['password'], is_pharmacy=data["is_pharmacy"], is_active=True) 
-    else:
+    if data["is_pharmacy"] not in [True, False]:
         return jsonify({"message":"You need to be a pharmacy or user. Get out."}), 403
     
-    # new_user = Users(email=data['email'], password=data['password'], is_pharmacy=data["is_pharmacy"], is_active=True)
-
-    db.session.add(new_user)
-    db.session.commit()
+    if data["is_pharmacy"] is False:
+        new_user = Users(email=data['email'], password=data['password'], is_pharmacy=data["is_pharmacy"], is_active=True) 
+        
+        db.session.add(new_user)
+        db.session.commit()
+        
+        if 'name' not in data:
+            return jsonify({"message": "Where is your name?"})
+        
+        new_patient = Patients(name=data['name'], users=new_user)
+        
+        db.session.add(new_patient)
+        db.session.commit()
+    
+    else:
+    
+        return jsonify({"message": "Still working on..."}, 503)
+    
+    
     return jsonify({"message": "User added successfully"}), 201
 
+    
 
 
+# React Router Outlet
 
 
--- src/front/js/pages/Register.jsx
+++ src/front/js/pages/Register.jsx
@@ -6,10 +6,12 @@ import { Navigate } from "react-router-dom";
 
 export const Register = () => {
     const { store, actions } = useContext(Context);
-    const [ email, setEmail ] = useState("");
-    const [ password, setPassword ] = useState("");
-    const [ is_pharmacy, setIs_pharmacy ] = useState(false)
-    
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [is_pharmacy, setIs_pharmacy] = useState(false)
+    const [name, setName] = useState("");
+
+
     // setEmailLogin("test");
     // setPasswordLogin("pass");
 
@@ -19,34 +21,38 @@ export const Register = () => {
         const url = process.env.BACKEND_URL + "/api/signup";
         console.log(url);
         const options = {
-            method : "POST",
-            body : JSON.stringify({email, password, is_pharmacy}),
-            headers : {
-                "Content-Type" : "application/json"
+            method: "POST",
+            body: JSON.stringify({ email, password, is_pharmacy, name}),
+            headers: {
+                "Content-Type": "application/json"
             }
         };
         console.log(options);
-
-        const response = await fetch(url, options);
-        console.log(response);
-        if(response.ok){
-            const data = await response.json();
-            console.log(data);
-            const msg = document.querySelector("#allDoneMessage");
-            const btn = document.querySelector("#send");
-            btn.disabled = true;
-            msg.style.display = "block";
-            setTimeout(() => {
-                window.location.href = "/login"
-            }, 2000)
-        }else{
-            const msg = document.querySelector("#errorMessage");
-            msg.style.display = "block";
-            setTimeout(() => {
-                msg.style.display = "none";
-            }, 3000)
-            console.log(" Falló...", response.status, response.statusText)
-        }
+        // try{
+            const response = await fetch(url, options);
+            console.log(response);
+            if (response.ok) {
+                const data = await response.json();
+                console.log(data);
+                const msg = document.querySelector("#allDoneMessage");
+                const btn = document.querySelector("#send");
+                btn.disabled = true;
+                msg.style.display = "block";
+                setTimeout(() => {
+                    window.location.href = "/login"
+                }, 2000)
+            } else {
+                const msg = document.querySelector("#errorMessage");
+                msg.style.display = "block";
+                setTimeout(() => {
+                    msg.style.display = "none";
+                }, 6000)
+                console.log(" Falló...", response.status, response.statusText)
+            }
+        // }catch(error){
+        //     console.log("An error has ocurred -> ",error)
+        // }
+        
 
 
     }
@@ -54,47 +60,54 @@ export const Register = () => {
 
 
     return (
-        store.isLoggedIn ? <Navigate to="/login"/>:
-        <div>
+        store.isLoggedIn ? <Navigate to="/login" /> :
+            <div>
 
-            <form onSubmit={handleSubmit} className="form-group col-md-6 py-5 px-md-5">
-                <h1 className="text-center">THIS IS REGISTER</h1>
-                <p id="allDoneMessage" className="p-3" style={{color: "green", borderRadius: "10px", border: "solid green 3px", display: "none"}}>Register completed</p>
-                {/* <p id="errorMessage" className="p-3" style={{color: "red", borderRadius: "10px", border: "solid red 3px", display: "none"}}>The pass or user doesn't exists</p> */}
-                <div className="form-outline mb-4">
-                    <input type="email" id="registerForm1" className="form-control" 
-                        value={email} onChange={(e) => setEmail(e.target.value)}/>
-                    <label className="form-label" htmlFor="registerForm1">Email address</label>
-                </div>
-                <div className="form-outline mb-4">
-                    <input type="password" id="registerForm2" className="form-control"
-                        value={password} onChange={(e) => setPassword(e.target.value)}/>
-                    <label className="form-label" htmlFor="registerForm2">Password</label>
-                </div>
-                <div className="form-outline mb-4">
-                    <input type="checkbox" id="registerForm3" className="form-check-input"
-                        onChange={(e) => setIs_pharmacy(e.target.checked)} checked={is_pharmacy}/>
-                    <label className="form-label" htmlFor="registerForm3">Is pharmcy?</label>
-                </div>
-
-                {is_pharmacy && (
+                <form onSubmit={handleSubmit} className="form-group col-md-6 py-5 px-md-5">
+                    <h1 className="text-center">THIS IS REGISTER</h1>
+                    <p id="allDoneMessage" className="p-3" style={{ color: "green", borderRadius: "10px", border: "solid green 3px", display: "none" }}>Register completed</p>
+                    <p id="errorMessage" className="p-3" style={{color: "red", borderRadius: "10px", border: "solid red 3px", display: "none"}}>Error during the register of new user</p>
                     <div className="form-outline mb-4">
-                        <select id="pharmacies" className="form-control">
-                            <option>Pharma de Móstoles</option>
-                            <option>Pharma de Dña manolita</option>
-                        </select>
+                        <input type="email" id="registerForm1" className="form-control"
+                            value={email} onChange={(e) => setEmail(e.target.value)} required />
+                        <label className="form-label" htmlFor="registerForm1">Email address</label>
                     </div>
-                )}
+                    <div className="form-outline mb-4">
+                        <input type="password" id="registerForm2" className="form-control"
+                            value={password} onChange={(e) => setPassword(e.target.value)} required />
+                        <label className="form-label" htmlFor="registerForm2">Password</label>
+                    </div>
+                    <div className="form-outline mb-4">
+                        <input type="checkbox" id="registerForm3" className="form-check-input"
+                            onChange={(e) => setIs_pharmacy(e.target.checked)} checked={is_pharmacy} />
+                        <label className="form-label" htmlFor="registerForm3">Is pharmcy?</label>
+                    </div>
 
+                    {!is_pharmacy && (
+                        <div className="form-outline mb-4">
+                            <h3>You are a patient</h3>
+                            <input type="text" id="registerForm4" className="form-control"
+                                value={name} onChange={(e) => setName(e.target.value)} required />
+                            <label className="form-label" htmlFor="registerForm4">Name</label>
+                        </div>
+                    )}
 
-                <div>
-                    <button id="send" type="submit" className="btn btn-primary btn-block mb-4">
-                        Sign up
-                    </button>
-                </div>
-            </form>
+                    {is_pharmacy && (
 
-        </div>
+                        <div>
+                            <h3>You are a pharmacy</h3>
+                        </div>
+
+                    )}
+
+                    <div>
+                        <button id="send" type="submit" className="btn btn-primary btn-block mb-4">
+                            Sign up
+                        </button>
+                    </div>
+                </form>
+
+            </div>
 
     )
 
