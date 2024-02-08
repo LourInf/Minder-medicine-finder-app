71ba378d4f705636f95f043863bbf807228edc2d
Evelyn <130219194+EveyZeram@users.noreply.github.com>
2024-02-08 20:27:21 +0000

    styles Details Pharmacy

--- src/front/js/component/DetailsPharmacyMaps.jsx
+++ src/front/js/component/DetailsPharmacyMaps.jsx
@@ -2,7 +2,7 @@ import React, { useContext, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import { Context } from "../store/appContext";
 import { Link } from "react-router-dom";
-import  Spinner  from 'react-bootstrap/Spinner';
+import Spinner from 'react-bootstrap/Spinner';
 
 export const DetailsPharmacyMaps = () => {
     const { store, actions } = useContext(Context);
@@ -10,28 +10,36 @@ export const DetailsPharmacyMaps = () => {
 
     return (
         <div>
-            <Link className="navbar-brand m-3" to="/maps">Volver</Link>
-            <h1 className="text-center p-2 m-3">Detalle de la Farmacia</h1>
+            <Link className="navbar-brand m-3 p-3" to="/maps">Volver</Link>
+            <h1 className="text-center p-2 m-3">Consulta los datos de la Farmacia</h1>
             {/* Si existe "pharmacydetails" entonces me extraes la info */}
             {pharmacyDetails ? (
-                <div className="container bg-success">
-                    <div className="card mb-3  bg-success text-light">
+                <div className="container">
+                    <div className="card mb-3">
                         <div className="row g-0">
                             <div className="col-md-7 col-lg-6 col-xl-5">
                                 <img
-                                    src="https://images.vexels.com/media/users/3/136559/isolated/preview/624dd0a951a1e8a118215b1b24a0da59-logotipo-de-farmacia.png"
-                                    className="card-img-top"
-                                    style={{ width: '80px', height: '80px' }}
+                                    src="https://www.carmona.org/ciudad/farmacias/logoFarmacias.png"
+                                    className="card-img-center"
                                     alt="farmacia"
                                 />
                             </div>
                             <div className="col-md-5 col-lg-6 col-xl-7">
-                                <div className="card-body">
-                                    <h1>{pharmacyDetails.place_id}</h1>
-                                    <h1>{pharmacyDetails.name}</h1>
+                                <div className="card-body w-75 text-center">
+                                    {/* <h1>{pharmacyDetails.place_id}</h1> */}
+                                    <h3>{pharmacyDetails.name}</h3>
                                     <p> Dirección: {pharmacyDetails.formatted_address}</p>
-                                    <p>Teléfono: {pharmacyDetails.formatted_phone_number}</p>
-                                    <p>Horario Laboral: {pharmacyDetails.current_opening_hours.weekday_text}</p>
+                                    <p>
+                                        Teléfono:{" "}
+                                        <a href={`tel:${pharmacyDetails.formatted_phone_number}`}>
+                                            {pharmacyDetails.formatted_phone_number}
+                                        </a>
+                                    </p>
+                                    <ul>
+                                        {pharmacyDetails.current_opening_hours.weekday_text.map((day, index) => (
+                                            <li className="list-inline" key={index}>{day}</li>
+                                        ))}
+                                    </ul>
                                     <p>Estado: {pharmacyDetails.current_opening_hours.open_now ? 'Abierto Ahora' : 'Cerrado'}</p>
                                 </div>
                             </div>
@@ -47,7 +55,7 @@ export const DetailsPharmacyMaps = () => {
                             <p>Cargando...</p>
                         </>
                     ) : (
-                        // si no hay detalles... 
+                        // Si no hay detalles... 
                         <p>No Existen Datos Para Esta Farmacia</p>
                     )}
                 </div>
