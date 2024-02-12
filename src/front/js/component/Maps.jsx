cc3528f029f63ac0c62b2a9aa37c41e0e0c0fc36
Evelyn <130219194+EveyZeram@users.noreply.github.com>
2024-02-12 19:57:27 +0000

    maps

--- src/front/js/component/Maps.jsx
+++ src/front/js/component/Maps.jsx
@@ -1,5 +1,5 @@
-import React, { useContext, useEffect, useState } from 'react';
 import "../../styles/maps.css"
+import React, { useContext, useEffect, useState } from 'react';
 import { Context } from '../store/appContext.js';
 import { Link, useNavigate } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
@@ -124,7 +124,7 @@ export const Maps = () => {
             {
               checked ?
                 item.opening_hours.open_now ?
-                  <div className="container w-50 custom-transparentcard card-container" key={index} style={{ background: "#007085", }}>
+                  <div className="container w-50 custom-transparentcard card-container my-3 border" key={index} style={{ background: "#007085", borderRadius: "10px", boxShadow: "0px 4px 8px rgba(0, 0, 0, 0.2)" }}>
                     <h2 className=""  style={{ color: "##00bab9", }}>{item.name}</h2>
                     {item.opening_hours && (
                       <p className='text-light'>Abierto Ahora</p>
@@ -136,7 +136,7 @@ export const Maps = () => {
                   : null
                 :
                 // Si el filtro checked es false, entonces me muestras todas las farmacias.
-                <div className="container w-50 custom-transparentcard card-container" key={index} style={{ background: "#007085"}}>
+                <div className="container w-50 custom-transparentcard card-container my-3 border" key={index} style={{ background: "#007085", borderRadius: "10px", boxShadow: "0px 4px 8px rgba(0, 0, 0, 0.2)" }}>
                   <h2 className=""style={{ color: "##00bab9", }}>{item.name}</h2>
                   {/* Si open_now es true, entonces estableces "Abierto" si no "Cerrado" */}
                   {item.opening_hours && (
